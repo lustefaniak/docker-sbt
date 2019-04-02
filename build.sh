ea2e38a81e41ed9766a74ff4768cadbc0ef8f34b
Lukasz Stefaniak <lustefaniak@gmail.com>
2019-04-03 00:56:58 +0200

    Build multiple base images

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ -z "$BASE" ]]; then
+if [[ -z "$NAME" ]]; then
     echo "Must provide BASE in environment" 1>&2
     exit 1
 fi
@@ -15,19 +15,9 @@ if [[ -z "$SCALA_VERSION" ]]; then
     exit 1
 fi
 
-
-case "$BASE" in
-        graalvm)
-            BASE_IMAGE=lustefaniak/docker-graalvm:alpine-1.0.0-rc14.0
-            ;;
-        *)
-            echo "Unrecongnized BASE=${BASE}"
-            exit 1
-esac
-
 FULL_VERSION=$(git describe --tags)
 VERSION=${FULL_VERSION//v}
 
-echo "Building using BASE=$BASE BASE_IMAGE=$BASE_IMAGE SBT_VERSION=$SBT_VERSION SCALA_VERSION=$SCALA_VERSION"
+echo "Building using NAME=$NAME BASE_IMAGE=$BASE_IMAGE SBT_VERSION=$SBT_VERSION SCALA_VERSION=$SCALA_VERSION VERSION=$VERSION"
 
-docker build . --build-arg BASE_IMAGE="${BASE_IMAGE}"  --build-arg SBT_VERSION="$SBT_VERSION" --build-arg SCALA_VERSION="$SCALA_VERSION" -t "lustefaniak/sbt:${BASE}_${SBT_VERSION}_${SCALA_VERSION}_${VERSION}"
+docker build . --build-arg BASE_IMAGE="${BASE_IMAGE}"  --build-arg SBT_VERSION="$SBT_VERSION" --build-arg SCALA_VERSION="$SCALA_VERSION" -t "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}_${VERSION}"
--- push.sh
+++ push.sh
@@ -4,4 +4,6 @@ echo "$DOCKER_PASSWORD" | docker login -u lustefaniak --password-stdin
 FULL_VERSION=$(git describe --tags)
 VERSION=${FULL_VERSION//v}
 
-docker push "lustefaniak/sbt:${BASE}_${SBT_VERSION}_${SCALA_VERSION}_${VERSION}"
+docker push "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}_${VERSION}"
+docker tag "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}_${VERSION}" "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}"
+docker push "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}"
