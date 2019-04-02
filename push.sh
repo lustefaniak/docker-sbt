10dd0213355ddb748cafefcc5f5ec94f37543e16
Lukasz Stefaniak <lustefaniak@gmail.com>
2019-04-02 11:30:34 +0200

    push.sh

---
+++ push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+FULL_VERSION=$(git describe --tags)
+VERSION=${FULL_VERSION//v}
+
+docker push "lustefaniak/sbt:${BASE}_${SBT_VERSION}_${SCALA_VERSION}_${VERSION}"
