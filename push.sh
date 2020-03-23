36b8e86a7ffa717931fdc80798e9ee8eb7e20e95
Lukasz Stefaniak <lustefaniak@gmail.com>
2020-03-23 23:51:31 +0100

    Should work fine now

--- push.sh
+++ push.sh
@@ -6,5 +6,5 @@ if [[ -z "$VERSION" ]]; then
 fi
 
 docker push "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}_${VERSION}"
-#docker tag "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}_${VERSION}" "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}"
-#docker push "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}"
+docker tag "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}_${VERSION}" "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}"
+docker push "lustefaniak/sbt:${NAME}_${SBT_VERSION}_${SCALA_VERSION}"
